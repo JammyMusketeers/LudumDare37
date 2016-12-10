d2606c78b591e777c2080639a458d8a5031595a7
Conna <kurozael@gmail.com>
2016-12-10 13:36:09 +0000

    Basic character movement

---
+++ Assets/Scripts/Camera/IsometricCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IsometricCamera : MonoBehaviour
+{
+	public Player player;
+	public float distance = 10f;
+
+	protected virtual void Update()
+	{
+		transform.position = player.gameObject.transform.position + new Vector3(-distance, distance, -distance);
+
+		transform.LookAt(player.gameObject.transform);
+
+		var rotation = transform.eulerAngles;
+
+		rotation.y = 45f;
+
+		transform.eulerAngles = rotation;
+	}
+}
