2ca53b1cd86cf64c74bf39b846c8e4c04bbe975e
Michael Loomes <mikeloomes@hotmail.com>
2016-12-11 18:43:39 +0000

    Stormy

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -36,17 +36,11 @@ public class GameManager : Singleton<GameManager>
 
 	private float _nextUpdateGround;
 	private float _nextHungerDecrease = 3f;
-	private float _nextStormChecker;
 	private List<Chunk> _chunks;
 
 	private Action _fadeToBlackCallback;
 	private float _targetFadeAlpha;
 	private bool _isFading;
-
-	private bool stormLevel0 = true;
-	private bool stormLevel1 = false;
-	private bool stormLevel2 = false;
-	private bool stormlevel3 = false;
 
 	public void FadeToBlack(Action callback = null)
 	{
@@ -239,41 +233,6 @@ public class GameManager : Singleton<GameManager>
 			_nextUpdateGround = Time.time + 1f;
 		}
 
-		//Check how far Storm is
-		if(Time.time >= _nextStormChecker)
-		{
-
-			var distance = Vector3.Distance(storm.transform.position, CurrentTram.transform.position);
-			
-			if(distance >= 900)
-			{
-				stormLevel0 = true;
-				stormLevel1 = false;
-
-			}
-
-			if(distance < 900f && distance >= 500f)
-			{
-				stormLevel1 = true;
-				stormLevel2 = false;
-			}
-
-			if(distance < 500f && distance >= 200f)
-			{
-				stormLevel1 = false;
-				stormLevel2 = true;
-				stormlevel3 = false;
-			}
-
-			if(distance < 200f)
-			{
-				stormLevel2 = false;
-				stormlevel3 = true;
-			}
-
-			_nextStormChecker = Time.time + 1f;
-
-		}
 		
 		interfaceManager.SetSpeed(CurrentTram.GetSpeedPerSecond());
 		interfaceManager.SetDistance(CurrentTram.transform.position.z);
@@ -282,7 +241,7 @@ public class GameManager : Singleton<GameManager>
 		interfaceManager.SetFuelConsumption(CurrentTram.GetFuelConsumption());
 		interfaceManager.SetFuel(CurrentTram.fuel / 100f);
 
-		Debug.Log("Storm Distance = " + Vector3.Distance(storm.transform.position, CurrentTram.transform.position));
+		
 		
 	}
 }
---
+++ Assets/Scripts/Managers/StormManager.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StormManager : Singleton<GameManager> {
+
+	public PlayerControl playerControl;
+	public Player player;
+	public GameObject storm;
+
+	private bool stormLevel0 = true;
+	private bool stormLevel1 = false;
+	private bool stormLevel2 = false;
+	private bool stormLevel3 = false;
+	private float _nextStormChecker;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Check how far Storm is
+		if(Time.time >= _nextStormChecker)
+		{
+
+			var distance = Vector3.Distance(storm.transform.position, player.transform.position);
+			
+			if(distance >= 900)
+			{
+				stormLevel0 = true;
+				stormLevel1 = false;
+
+			}
+
+			if(distance < 900f && distance >= 500f)
+			{
+				stormLevel1 = true;
+				stormLevel2 = false;
+			}
+
+			if(distance < 500f && distance >= 200f)
+			{
+				stormLevel1 = false;
+				stormLevel2 = true;
+				stormLevel3 = false;
+			}
+
+			if(distance < 200f)
+			{
+				stormLevel2 = false;
+				stormLevel3 = true;
+			}
+			
+			if (stormLevel0)
+			{
+				playerControl.stormSpeedDecay = 1f;
+			}
+
+			if (stormLevel1)
+			{
+				playerControl.stormSpeedDecay = 0.9f;
+			}
+
+			if (stormLevel2)
+			{
+				playerControl.stormSpeedDecay = 0.7f;
+			}
+
+			if (stormLevel3)
+			{
+				playerControl.stormSpeedDecay = 0.5f;
+			}
+
+			Debug.Log("Storm Distance = " + distance);
+			_nextStormChecker = Time.time + 1f;
+
+		}
+	
+	}
+}
