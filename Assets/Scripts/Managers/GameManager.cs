e4b7d2443838e3a69cfa5606973c60ccc6cf106b
Michael Loomes <mikeloomes@hotmail.com>
2016-12-12 23:43:28 +0000

    balance

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -138,7 +138,11 @@ public class GameManager : Singleton<GameManager>
 		if (addLoot)
 		{
 			chunk.AddLoot();
-			chunk.AddObjects();
+			if(x != 0)
+			{
+				chunk.AddObjects();
+			}
+			
 		}
 
 		if (x != 0)
